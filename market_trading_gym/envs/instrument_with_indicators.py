76d17df987e352157f3bb0573ba1fc33f3791ee6
westonplatter <westonplatter@gmail.com>
2021-01-18 16:00:50 -0700

    working proptype working with VWMA and Diff

--- market_trading_gym/envs/instrument_with_indicators.py
+++ market_trading_gym/envs/instrument_with_indicators.py
@@ -2,9 +2,6 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
-from pathlib import Path
-
-import csv
 import enum
 import glob
 import os
@@ -13,10 +10,16 @@ import numpy as np
 import random
 
 
-MAX_ACCOUNT_BALANCE = 100_000
-INITIAL_ACCOUNT_BALANCE = 10_000
+# MAX_ACCOUNT_BALANCE = 100_000
+# INITIAL_ACCOUNT_BALANCE = 10_000
 LOOKBACK_INTERVAL = 6
-MAX_STEPS = 1_000
+# MAX_STEPS = 1_000
+MAX_ACCOUNT_BALANCE = 100_000
+MAX_NUM_SHARES = 200
+MAX_SHARE_PRICE = 20_000
+MAX_OPEN_POSITIONS = 3
+MAX_STEPS = 10500 #20000
+INITIAL_ACCOUNT_BALANCE = 10_000
 
 
 class Fields(enum.Enum):
@@ -25,6 +28,8 @@ class Fields(enum.Enum):
     LOW = "Low"
     CLOSE = "Close"
     VOLUME = "Volume"
+    VWMA = "VWMA"
+    DIFF = "Diff"
 
 
 class InstrumentWithIndicators(gym.Env):
@@ -33,14 +38,14 @@ class InstrumentWithIndicators(gym.Env):
     def __init__(self, df):
         super(InstrumentWithIndicators, self).__init__()
         self.df = df
-        self.reward_range(0, MAX_ACCOUNT_BALANCE)
+        self.reward_range = (0, MAX_ACCOUNT_BALANCE)
 
         self.action_space = spaces.Box(
-            low=np.array([0, 0]), high=np.array[3, 1], dtype=np.float16
+            low=np.array([0, 0]), high=np.array([3, 1]), dtype=np.float16
         )
 
         self.observation_space = spaces.Box(
-            low=0, high=1, shape=(6, 6), dtype=np.float16
+            low=0, high=1, shape=(8, 6), dtype=np.float16
         )
 
     def reset(self, **kwargs):
@@ -49,8 +54,8 @@ class InstrumentWithIndicators(gym.Env):
         self.max_net_worth = INITIAL_ACCOUNT_BALANCE
         self.shares_held = 0
         self.cost_basis = 0
-        self.total_shares_held = 0
-        self.total_shares_value = 0
+        self.total_shares_sold = 0
+        self.total_sales_value = 0
 
         self.current_step = random.randint(
             0, len(self.df.loc[:, Fields.OPEN.value].values) - LOOKBACK_INTERVAL
@@ -62,39 +67,38 @@ class InstrumentWithIndicators(gym.Env):
         mmin = self.current_step
         mmax = self.current_step + (LOOKBACK_INTERVAL - 1)
 
-        frame = np.array[
-            self.df.loc[mmin:mmax, Fields.OPEN.value].values,
-            self.df.loc[mmin:mmax, Fields.HIGH.value].values,
-            self.df.loc[mmin:mmax, Fields.LOW.value].values,
-            self.df.loc[mmin:mmax, Fields.CLOSE.value].values,
-            self.df.loc[mmin:mmax, Fields.VOLUME.value].values,
-        ]
+        def get_values_for(field_key: Fields):
+            return self.df.loc[mmin:mmax, field_key.value].values
 
-        obs = np.append(
-            frame,
-            [
-                [
-                    self.balance,
-                    self.max_net_worth,
-                    self.shares_held,
-                    self.cost_basis,
-                    self.total_shares_held,
-                    self.total_shares_value,
-                ]
-            ],
-            axis=0,
-        )
+        frame = np.array([
+            get_values_for(Fields.OPEN),
+            get_values_for(Fields.HIGH),
+            get_values_for(Fields.VOLUME),
+            get_values_for(Fields.LOW),
+            get_values_for(Fields.CLOSE),
+            get_values_for(Fields.VWMA),
+            get_values_for(Fields.DIFF),
+        ])
+
+        account_data = [[
+            self.balance / MAX_ACCOUNT_BALANCE,
+            self.max_net_worth / MAX_ACCOUNT_BALANCE,
+            self.shares_held / MAX_NUM_SHARES,
+            self.cost_basis / MAX_SHARE_PRICE,
+            self.total_shares_sold / MAX_NUM_SHARES,
+            self.total_sales_value / (MAX_NUM_SHARES * MAX_SHARE_PRICE),
+        ]]
+        
+        obs = np.append(frame, account_data, axis=0)
 
         return obs
 
     def step(self, action):
-        self._take_action(self, action)
+        self._take_action(action)
 
         self.current_step += 1
 
-        if self.current_step > len(
-            self.df.loc[:, Fields.OPEN.value].values - LOOKBACK_INTERVAL
-        ):
+        if self.current_step > len(self.df.loc[:, Fields.OPEN.value].values) - LOOKBACK_INTERVAL:
             self.current_step = 0
 
         delay_modifier = self.current_step / MAX_STEPS
@@ -132,8 +136,8 @@ class InstrumentWithIndicators(gym.Env):
             shares_sold = self.shares_held * amount
             self.balance += shares_sold * current_price
             self.shares_held -= shares_sold
-            self.total_shares_sold += share_sold
-            self.total_shares_value += shares_sold * current_price
+            self.total_shares_sold += shares_sold
+            self.total_sales_value += shares_sold * current_price
 
         self.net_worth = self.balance + (self.shares_held * current_price)
 
@@ -147,7 +151,7 @@ class InstrumentWithIndicators(gym.Env):
         profit = self.net_worth - INITIAL_ACCOUNT_BALANCE
 
         print(f"Shares held: {self.shares_held}")
-        print(f"Total sold: {self.total_shares_sold}")
+        # print(f"Total sold: {self.total_shares_sold}")
         print(f"Avg cost for held shares: {self.cost_basis}")
         print(f"Total sales value: {self.total_sales_value})")
         print(f"Net worth: {self.net_worth}. Max net worth: {self.max_net_worth})")
--- run_example.py
+++ run_example.py
@@ -1,23 +1,54 @@
-import os
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+# https://towardsdatascience.com/creating-a-custom-openai-gym-environment-for-stock-trading-be532be3910e
+ 
 
+# external libs
 import datetime as dt
 import gym
-import pandas as pd
 import numpy as np
-
+import os
+import pandas as pd
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
 
+# internal libs
 from market_trading_gym.envs import InstrumentWithIndicators
 
+# fixes MPI issue
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 
-def prepare_df(df: pd.DataFrame) -> pd.DataFrame:
-    # TODO(weston) - merge date/time fields into 1 python datetime field
-    return df
 
-df = pd.read_csv('data/m2k-renko.csv')
+
+def prepare_df(ddf: pd.DataFrame) -> pd.DataFrame:
+    # format headers
+    ddf.columns = [x.strip() for x in ddf.columns]
+    ddf.rename(columns={"Last": "Close"}, inplace=True)
+
+    # convert Date(str) and Time(str) into datetime column
+    time_values = df.Date + df.Time
+    ddf["Date"] = pd.to_datetime(
+        time_values, format="%Y/%m/%d %H:%M:%S.%f", errors="coerce"
+    )
+
+    # remove data before roll period for the March 2021 contract
+    ddf = ddf.query("Date > '2020-12-15'")
+
+    # extract the specific RL model columns Date, OHLC, and Volume
+    desired_columns = ["Date", "Open", "High", "Low", "Close", "Volume", "VWMA", "Diff"]
+    ddf = ddf[desired_columns].copy()
+    ddf.sort_values("Date", ignore_index=True, inplace=True)
+    return ddf
+
+
+df = pd.read_csv("data/m2k-renko.csv")
 df = prepare_df(df)
+env = DummyVecEnv([lambda: InstrumentWithIndicators(df)])
 
-# https://towardsdatascience.com/creating-a-custom-openai-gym-environment-for-stock-trading-be532be3910e
+model = PPO2(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=10_000)
+
+obs = env.reset()
+for i in range(20_000):
+  action, _states = model.predict(obs)
+  obs, rewards, done, info = env.step(action)
+  env.render()
